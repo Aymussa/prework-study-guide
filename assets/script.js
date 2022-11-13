10e7be5fa4d0a8cb7d44ed60ffad88e1b1df96b0
unknown <aymmussa@gmail.com>
2022-11-13 23:42:34 +0000

    completed the git flow

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,26 @@
-/* if topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
-*/
 
-
-
-var topics = ["HTML", "CSS" , "Git", "JavaScript"];
-
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-    
-}
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
+console.log('Here are the topic we learn through Prework');
+listTopics();
+console.log('Which topic should we study fist?');
+selectTopic();
